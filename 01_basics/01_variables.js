6598871a1450955c323dcfc010b8b78c9134a75a
Rubrangso Kri <112789870+R-Kri@users.noreply.github.com>
2023-12-18 19:04:43 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 14455
+let accountEmail = "hello@google.com"
+var accountPassword = "123456"
+accountCity = "Jaipur" // possible but not a convinient way
+let accountState;
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/ 
+// accountId=2 //Constant keyword cannot be changed
+accountEmail = "world@gmail.com"
+accountPassword = "173823"
+accountCity = "Delhi"
+console.log(accountId);
+
+// To print multiple variables we can use console.table 
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //We are using nodejs not browser therefore can't be use.
+
+console.log(3+
+    3) // (Don't write like this) code readability should be high.
+let age=18;
+
+/*
+DATA TYPES
+number => upto 2 to power 53
+bigint (for very very lrge number of data)
+string => ""
+boolean => true or false
+null => stand alone value (represents empty space)
+undefined => value is not assigned yet
+symbol => To define uniqueness
+object 
+*/
+
+console.log(typeof age);
+console.log(typeof undefined); //datatype is undefined
+console.log(typeof null); // datatype of null is object
