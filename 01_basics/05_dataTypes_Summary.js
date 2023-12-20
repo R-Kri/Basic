4cd17d93d1e706103106bfe151698c205b5ff7dd
Rubrangso Kri <112789870+R-Kri@users.noreply.github.com>
2023-12-20 14:50:08 +0000

    Datatype summary

---
+++ 01_basics/05_dataTypes_Summary.js
@@ -0,0 +1,33 @@
+// How do store data in memory and access from there on that basis
+// two datatypes were created i) Primitive ii) Non-primitive or reference type
+
+/* PRIMITIVE - Call by value
+7 categories - String, Number, Boolean, null, undefined, Symbol, BigInt
+*/
+
+/* NON-PRIMITIVE - Call by reference
+Array, Objects, Functions */
+
+/*JavaScript is a dynamically typed language. In dynamically typed languages, the data type of variables is determined by the value they hold at runtime, and can change throughout the program as we assign different values to them. There's no inbuilt way to annotate or restrict the types of variables.*/
+
+// In JS we don't differentiate btw integer and decimal,(a number is number)
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId) // False cause symbol gives unique idetifier to everyone.
+
+const heroes = ["shaktiman","Nagraj"]; // for arrays
+
+// For Objects 
+let myObj = {
+    name: "Thor",
+    age: 21,
+}
+
+// For Functions
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+//Datatype of null shows "Object" when we use typeof function to get datatype.
