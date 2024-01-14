1e55ba58086660efd82ea02f34d927c80362bffd
Rubrangso Kri <112789870+R-Kri@users.noreply.github.com>
2024-01-14 12:38:15 +0000

    Arrays

--- 02_basics_part02/01_array.js
+++ 02_basics_part02/01_array.js
@@ -1,1 +1,43 @@
-Starting arrays
+// Starting arrays
+// Declarations of array.
+const myArr = [0,1,2,3,4,5]
+const myHeroes = ["shaktiman","Naaga"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr2[1]);
+
+// Array Methods
+
+myArr.push(6) // push => It adds elements in the array.
+myArr.push(7)
+
+myArr.pop() // pop will remove the last value
+
+myArr.unshift(9) // It'll add the element in the first position
+
+myArr.shift() // Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+console.log(myArr);
+
+// There are some questionnaire methods in JS, we used it to ask questions. For e.g
+
+console.log(myArr.includes(9)); // It'll check if the array includes 9 or not. If it do then it'll return "true" otherwise "false"
+
+console.log(myArr.indexOf(9)); // It'll tell us the index of the element, and if it doesn't exist then it'll return -1.
+
+// Important
+const newArr = myArr.join() //Adds all the elements of an array into a string, separated by the specified separator string.
+console.log(myArr);
+console.log(newArr); // Both the console will print almost same output but the major change was done in the type. "join" binded the array in form 0,1,2,3,4,5,6 and converts the array into string. 
+
+// SLICE AND SPLICE
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3) //Returns a copy of a section of an array. For both start and end, a negative index can be used to indicate an offset from the end of the array. 
+console.log(myn1);
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,3) // Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+console.log("C ",myArr);
+console.log(myn2); 
+
+// NOTE: slice returns a piece of the array but it doesn't affect the original array. splice changes the original array by removing, replacing, or adding values and returns the affected values.
---
+++ 02_basics_part02/02_array.js
@@ -0,0 +1,42 @@
+const marvel_heroes = ["Thor","Ironman","Spiderman"]
+const dc_heroes = ["Superman","Flash","Batman"]
+
+// marvel_heroes.push(dc_heroes) // push adds different types of datatypes it isn't necessary to be a particular same datatype.
+// console.log(marvel_heroes); // [ 'Thor', 'Ironman', 'Spiderman', [ 'Superman', 'Flash', 'Batman' ] ] => Treats the dc_heroes array as 4th element.
+// console.log(marvel_heroes[3][1]); // Flash
+
+// // Concat: Combines two or more arrays. This method returns a new array without modifying any existing arrays.
+
+// marvel_heroes.concat(dc_heroes)
+// console.log(marvel_heroes); // [ 'Thor', 'Ironman', 'Spiderman', [ 'Superman', 'Flash', 'Batman' ] ] 
+
+// To merge both the arrays properly.
+
+const allHeroes = marvel_heroes.concat(dc_heroes)
+console.log(allHeroes);
+
+// Alternately we can use spread operator.
+
+const all_new_heroes = [...marvel_heroes,...dc_heroes] // This is our spread operator. More prefferable cause we can add another array with ...third_array method.
+console.log(all_new_heroes);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity) // Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+console.log(real_another_array); // All the brackets under array will be removed and treated as a single element. Output: [1,2,3,4,5,6,7,6,7,4,5]
+
+// We can also ask is it array or not using ".isArray":
+console.log(Array.isArray("Hitesh")); // False
+
+// But what is we want to convert it into an array. We can perform it using ".from" :
+console.log(Array.from("Hitesh")); // [ 'H', 'i', 't', 'e', 's', 'h' ] => Changed into an array
+
+// If it couldn't be able to create an array then it'll return an empty array.
+// Intresting case
+console.log(Array.from({name: "Hitesh"})); // []
+
+// To convert into arrays.
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
