6e24ab57a815594a075cd26cb0c77b1ad1b62b22
Rubrangso Kri <112789870+R-Kri@users.noreply.github.com>
2024-01-30 08:08:08 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,12 +1,43 @@
 
-// {} This is scope, when it comes with functions and if-else statements
-if (true) {
-    let a = 10
-    const b = 20
-    var c = 30
-}
-console.log(a); // No output as "a" is only defined under if 
-console.log(b); // No output as "a" is only defined under if 
-console.log(c); // prints 30 which is a problem. Hence it is better to avoid var.
+// // // {} This is scope, it comes with functions and if-else statements
+// // if (true) {
+// //     let a = 10
+// //     const b = 20
+// //     var c = 30
+// // }
+// // console.log(a); // No output as "a" is only defined under if 
+// // console.log(b); // No output as "a" is only defined under if 
+// // console.log(c); // prints 30 which is a problem. Hence it is better to avoid var.
 
-// Study further about block scope and global scope.
+// // Study further about block scope and global scope.
+
+// function one(){
+//     const username = "hitesh"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     console.log(website);// Error cause it cannot access website variable outside the scope of the function "two()". 
+//     two()
+// } // When we have a nested function a child function can access the variable of the parent function, but vice versa is not true.
+// one()
+
+// +++++++++++++++++++++ INTRESTING ++++++++++++++++++++++++
+
+console.log(addOne(5));// Output : 6
+
+function addOne(num){
+    return num + 1
+}
+
+console.log(addOne(5));
+
+addTwo(5)// It will give error cause we cannot precall it in this (2nd type) of declaration. Declaaration se pehle hum use nahi kr skte when we declare the function with second method.
+
+const addTwo = function(num){ // Here we have declared the function but we have also hold it into a variable.
+    return num + 2
+}
+
+addTwo(5)
+// In both ways we can declare a function
